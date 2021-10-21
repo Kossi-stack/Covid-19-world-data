f2651e5fad945ece13daa787e1cd2689208e8993
Kossi-stack <81444338+Kossi-stack@users.noreply.github.com>
2021-10-21 12:33:49 -0700

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="footer">
+    <p className="footer-text">
+      Created with 💖 by Kossi-Stack as my React Capstone.
+    </p>
+  </footer>
+);
+
+export default Footer;
